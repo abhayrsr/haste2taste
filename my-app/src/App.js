7a265cac0a6dd2dad63f7140e6b14f7ed1236956
abhayrsr <abhayrajsinghrana123@gmail.com>
2023-10-24 16:25:35 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Home from "./screens/Home";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Home />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
