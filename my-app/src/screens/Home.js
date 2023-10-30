2ceef231dcd40f037b43a6f98d84d74413509d14
abhayrsr <abhayrajsinghrana123@gmail.com>
2023-10-30 22:44:43 +0530

    refactor home

--- my-app/src/screens/Home.js
+++ my-app/src/screens/Home.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Card from "../components/Card";
 import Footer from "../components/Footer";
 import Carousel from "../components/Carousel";
 
 export default function Home() {
+
+  const [foodCat, setFoodCat] = useState([]);
+  const [foodItem, setFoodItem] = useState([]);
+
+  const loadData = async() => {
+    let response = await fetch("http://localhost:5000/api/foodData", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    response = await response.json();
+    setFoodCat(response[0]);
+    setFoodItem(response[1])
+    console.log(response[0], response[1]);
+  }
+
+
+  useEffect(() => {
+    loadData()
+  }, [])
+
+
+
+
   return (
     <div>
       <div>
